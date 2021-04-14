113fc990508e94c083054c18291908c1f068ffa6
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-04-14 16:56:49 +0600

    cons

--- pracc.js
+++ pracc.js
@@ -4,4 +4,4 @@ const another=
 thank you for helping me out 
 regards 
 john`                           //  ${placeholder} this will allow to //${2+4} can be added to this and this will result 6 in output
-console.log(another);
+console.log(another);                                // this is the benefit of using template literals the backtik ``
