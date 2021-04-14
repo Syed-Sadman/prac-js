7bfc48c794646bb9848a879438dca79b73a32e6a
Syed Sadman Sabbir <sadman.syed@gmail.com>
2021-04-14 16:39:08 +0600

    first commit

---
+++ pracc.js
@@ -0,0 +1,7 @@
+const nme='sadman';
+const another=
+`hi ${nme},         
+thank you for helping me out 
+regards 
+john`                           //  ${placeholder} this will allow to 
+console.log(another);
