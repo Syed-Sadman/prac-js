87c7d6785f1dc4f426e8bad0ca700d2849d5a418
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-04-15 02:01:26 +0600

    object dec

--- pracc.js
+++ pracc.js
@@ -5,3 +5,28 @@ thank you for helping me out
 regards 
 john`                           //  ${placeholder} this will allow to //${2+4} can be added to this and this will result 6 in output
 console.log(another);                                // this is the benefit of using template literals the backtik ``
+
+
+
+
+// Object oriented programming in JavaScript 
+// without making it an object 
+let baseSalary=333;
+let overTime=22;
+let rate=444;
+
+function getWage(baseSalary,overTime,rate){
+    return baseSalary + (overTime*rate);
+}
+
+
+//same code as an object 
+let employee={
+    baseSalary: 333,
+    overTime:22,
+    rate:444,
+    getWage: function(){
+        return this.baseSalary + (this.overTime*this.rate);
+    }
+};
+employee.getWage();
