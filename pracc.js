b393d76bc8868e32cf5323b356d9d5bf475b2458
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-04-14 16:53:51 +0600

    literal

--- pracc.js
+++ pracc.js
@@ -1,7 +1,7 @@
 const nme='sadman';
 const another=
-`hi ${nme},         
+`hi ${nme} ${2+4},         
 thank you for helping me out 
 regards 
-john`                           //  ${placeholder} this will allow to 
+john`                           //  ${placeholder} this will allow to //${2+4} can be added to this and this will result 6 in output
 console.log(another);
