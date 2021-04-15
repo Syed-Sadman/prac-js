d9d2893262bb9131050d2de26672763697dd2c26
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-04-15 22:04:50 +0600

    arr

--- pracc.js
+++ pracc.js
@@ -1,4 +1,4 @@
-const nme='sadman';
+/*const nme='sadman';
 const another=
 `hi ${nme} ${2+4},         
 thank you for helping me out 
@@ -30,3 +30,65 @@ let employee={
     }
 };
 employee.getWage();
+
+
+//object defining and method
+let circle={
+    radius : 1,
+    location:{
+        x:1,
+        y:1
+    },
+    draw:function(){
+        console.log('draw');                 //draw:function is a method 
+    }
+ };
+circle.draw();
+*/
+
+//2 ways to create an object 1.factory function 2. constructor function 
+//factory function 
+/*
+function createCircle(radius){
+    return{
+        radius,
+        draw:function(){
+        console.log('draw');
+    }
+    
+    };
+}
+const circle=createCircle(1);
+
+//Constructor function 
+function Circle(radius){
+    this.radius=radius;
+    this.draw=function(){
+        console.log('draw');
+
+    }
+}
+
+const another= new Circle(1);
+*/
+
+let arr=[];
+
+let i=0;
+while(i<5){
+    arr.push(i);
+    i++;
+}
+
+let savearr=arr;
+console.log(savearr);
+
+
+let arr2=[];
+let j=5;
+while(j>=0){
+    arr2.push(j);
+    j--;
+}
+let savearr2=arr2;
+console.log(savearr2);
