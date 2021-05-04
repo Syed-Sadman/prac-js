d0134368b76ea8d206c1ba3546916ead70d4fec8
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-05-04 16:46:59 +0600

    modified

--- recjs.js
+++ recjs.js
@@ -1,18 +1,39 @@
-let p= new Promise((resolve,reject)=>{
-    let a=1+2
-    if(a==2){
-        resolve("valid");
-    }
-    else{
-        reject("invalid")
-    }
-});
+// let p= new Promise((resolve,reject)=>{
+//     let a=1+2
+//     if(a==2){
+//         resolve("valid");
+//     }
+//     else{
+//         reject("invalid")
+//     }
+// });
 
 
-p.then((mes)=>{
-    console.log(`this is the resolve  ${mes}`);
+// p.then((mes)=>{
+//     console.log(`this is the resolve  ${mes}`);
+// })
+
+// .catch((mes)=>{
+//     console.log(`this is the catch  ${mes}`);
+// })
+
+const recvid1=new Promise((resolve,reject)=>{
+    resolve("video 1 recorded")
 })
 
-.catch((mes)=>{
-    console.log(`this is the catch  ${mes}`);
+const recvid2=new Promise((resolve,reject)=>{
+    resolve("video 2 recorded")
+})
+
+const recvid3=new Promise((resolve,reject)=>{
+    resolve("video 3 recorded")
+})
+
+
+Promise.all([
+    recvid1,
+    recvid2,
+    recvid3
+]).then((mes)=>{
+    console.log(mes);
 })
