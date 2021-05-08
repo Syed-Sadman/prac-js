423da940c7b36c0baaa78b83cdf5d4e9ab3446f0
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-05-09 02:36:00 +0600

    this finished

--- this.js
+++ this.js
@@ -1,21 +1,47 @@
-var Person=function(name,age){
-    return {
-        name:name,
-        age:age,
-        printName:function(){
-            console.log(this.name);
-        },
-        father : {
-            name:"mr.xxx",
-            age:age,
-            printName: function(){
-                console.log(this.name);
-            }
-        }
-    };
-}
+// // 1. implicit bindings 
 
-var sakib=Person("SHAKib",34);
+// var Person=function(name,age){
+//     return {
+//         name:name,
+//         age:age,
+//         printName:function(){
+//             console.log(this.name);
+//         },
+//         father : {
+//             name:"mr.xxx",
+//             age:age,
+//             printName: function(){
+//                 console.log(this.name);
+//             }
+//         }
+//     };
+// }
 
-sakib.printName();
-sakib.father.printName();
+// var sakib=Person("SHAKib",34);
+
+// sakib.printName();
+// sakib.father.printName();
+
+
+
+
+//2. explicit bindings 
+
+var printNm=function(){
+    console.log(this.naam);
+} 
+
+
+
+
+var sakib={
+    naam:"Shakib",
+    age:34
+};
+
+
+printNm.call(sakib);                    //function.call(object name)  
+
+
+
+//3. new binding /////same as prototype new constructor 
