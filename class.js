1c88e16dca4e78871255181e33bbad55bbbb10f8
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-05-08 13:44:40 +0600

    k

--- class.js
+++ class.js
@@ -1,23 +1,23 @@
-// class Person{
-//     constructor(name,age){
-//         this.name=name;
-//         this.age=age;
-//     }
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
 
-//     eat(){
-//         console.log("person is eating")
-//     }
-//     play(){
-//         console.log("person is playing")
-//     }
-//     sleep(){
-//         console.log("person is sleeping")
-//     }
-// }
+    eat(){
+        console.log("person is eating")
+    }
+    play(){
+        console.log("person is playing")
+    }
+    sleep(){
+        console.log("person is sleeping")
+    }
+}
 
-// const newp=new Person("sakibddd",444);
+const newp=new Person("sakibddd",444);
 
-// newp.sleep();
+newp.sleep();
 
 
 
