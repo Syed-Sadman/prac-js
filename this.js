44c56bf615aa808426e3f6960afd6191ef668270
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-05-25 01:06:17 +0600

    this confusion cleared ALhamdulillah

--- this.js
+++ this.js
@@ -27,21 +27,70 @@
 
 //2. explicit bindings 
 
-var printNm=function(){
-    console.log(`${this.name} is ${this.age} years old and he is ${v}`);
-}; 
+// var printNm=function(){
+//     console.log(`${this.name} is ${this.age} years old and he is ${v}`);
+// }; 
 
 
 
 
-var sakib={
-    name:"Shakib",
-    age:34
-};
+// var sakib={
+//     name:"Shakib",
+//     age:34,
+//     method: function(){
+//         console.log(`${this.name} is ${this.age} years old `);
+//     },
+//     function(){
+//         console.log(this.method());
+//     }
+// };
 
-var v=['meritorious', 'humble','allrounder'];
-
-let p=printNm.bind(sakib,v);
-p();
+// sakib.function();
 
 //3. new binding /////same as prototype new constructor 
+
+
+
+
+
+
+/////////////  FAT ARROW => FUNCTION FOR clearing confusion of "this" KEYWORD
+
+// let jsr={
+//     name:'JavaScript',
+//     library : ['React', 'Angular','Vue'],
+//     printLibrary:function(){
+//         console.log(this);//this will refer to the jsr object
+//         this.library.forEach(function(a){
+//             console.log(this);  //window this or object this 
+//             console.log(`${this.name} has library like ${a}`);
+//         })
+//     }
+// }
+
+// jsr.printLibrary();
+
+
+
+
+
+////
+
+
+
+let jsr={
+    name:'JavaScript',
+    library : ['React', 'Angular','Vue'],
+    printLibrary:function(){
+        //console.log(this);
+        this.library.forEach((a)=>{
+         //   console.log(this);
+            console.log(`${this.name} has library like ${a}`);
+        })
+    }
+}
+
+jsr.printLibrary();
+
+
+
