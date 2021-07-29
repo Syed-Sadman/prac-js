7462a3e6c8cef716a5dae200ce3d8a5f6436c1e6
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-07-29 12:58:01 +0600

    map out object

--- map.js
+++ map.js
@@ -1,9 +1,19 @@
 var array=[1,2,3,4,5];
 
-var newArr=array.map((num)=>{
-    return 2*num;
-});
+
+//mapping through array indexes
+// var newArr=array.map((num)=>{
+//     return 2*num;
+// });
 
 
-console.log(array);
-console.log(newArr);
+// console.log(array);
+// console.log(newArr);
+
+
+//mapping as a object
+var objArr=array.map((n)=>{
+    return {value:n}
+})
+
+console.log(objArr);
